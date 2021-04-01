93ec176ce5cef686b2f478b0d7ea57588e9543d6
Karina Finegan <kairosity@users.noreply.github.com>
2021-04-01 16:26:55 +0100

    added adjust bag and remove from bag views, urls & templates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
 
 def view_bag(request):
     '''
@@ -46,3 +46,66 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    '''
+    Adjust the quantity of the specified product to the specified amount.
+    '''
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    # If the item has a size
+    if size: 
+        # If the adjusted quantity is more than 0 i.e. if the item has not been deleted.
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # If quantity is set to 0 then delete that item in that size.
+            del bag[item_id]['items_by_size'][size]
+            # If there are none of the same item in other sizes then just remove the item_id (item) completely.
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        # If the item doesn't have a size and the quantity is set to greater than 0
+        if quantity > 0:
+            # set the quantity to the quantity
+            bag[item_id] = quantity
+        else:
+            # If the quantity is set to 0 - then pop the item.
+            bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    '''
+    Adjust the quantity of the specified product to the specified amount.
+    '''
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        # If the item has a size
+        if size: 
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+            else:
+                # If quantity is set to 0 then delete that item in that size.
+                del bag[item_id]['items_by_size'][size]
+        else:
+            bag.pop(item_id)
+        
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
